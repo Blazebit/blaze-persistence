768c75017d6d1ecfbae45d6c20794c0733f25d06
Christian Beikov <christian.beikov@gmail.com>
2014-10-22 09:39:27 +0200

    Group by order issue fixed

--- core/impl/src/main/java/com/blazebit/persistence/impl/OrderByManager.java
+++ core/impl/src/main/java/com/blazebit/persistence/impl/OrderByManager.java
@@ -21,6 +21,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import javax.persistence.metamodel.Metamodel;
@@ -154,7 +155,7 @@ public class OrderByManager extends AbstractManager {
             return Collections.EMPTY_SET;
         }
 
-        Set<String> groupByClauses = new HashSet<String>();
+        Set<String> groupByClauses = new LinkedHashSet<String>();
         Iterator<OrderByInfo> iter = orderByInfos.iterator();
         OrderByInfo orderByInfo;
 
