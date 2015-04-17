e758a0808efe19eeaae2a995e49feb47b1eedc34
Christian Beikov <christian.beikov@gmail.com>
2015-04-17 17:54:31 +0200

    Fixed #115

---
+++ core/testsuite/src/test/java/com/blazebit/persistence/entity/PolymorphicSub1.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2014 Blazebit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.blazebit.persistence.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author Christian Beikov
+ * @since 1.0
+ */
+@Entity
+public class PolymorphicSub1 extends PolymorphicBase {
+    private static final long serialVersionUID = 1L;
+
+    private IntIdEntity relation1;
+
+    public PolymorphicSub1() {
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    public IntIdEntity getRelation1() {
+        return relation1;
+    }
+
+    public void setRelation1(IntIdEntity relation1) {
+        this.relation1 = relation1;
+    }
+}
