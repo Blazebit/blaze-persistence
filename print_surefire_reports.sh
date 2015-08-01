74a5e1c47efbde317d035dcbcff60c6b0758845c
Christian Beikov <christian.beikov@gmail.com>
2015-08-01 19:01:02 +0200

    Add print reports

---
+++ print_surefire_reports.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+echo "Current directory is $(pwd)"
+echo "\n=== SUREFIRE REPORTS ===\n"
+
+recurse() {
+	for i in "$1"/*; do
+		if [ -d "$i" ];then
+			print_report "$i"
+			recurse "$i"
+		fi
+	done
+}
+
+print_report() {
+	for F in $1/target/surefire-reports/*.txt; do
+		echo $F
+		cat $F
+		echo
+	done
+}
