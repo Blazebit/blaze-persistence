31871ddfd6172fe816a50c2f8823cd77be2c0516
Christian Beikov <christian.beikov@gmail.com>
2015-08-01 19:14:06 +0200

    Fix print script

--- print_surefire_reports.sh
+++ print_surefire_reports.sh
@@ -4,7 +4,7 @@ echo "\n=== SUREFIRE REPORTS ===\n"
 
 recurse() {
 	for i in "$1"/*; do
-		if [ -d "$i" ];then
+		if [ -d "$i" ]; then
 			print_report "$i"
 			recurse "$i"
 		fi
@@ -12,9 +12,13 @@ recurse() {
 }
 
 print_report() {
-	for F in $1/target/surefire-reports/*.txt; do
-		echo $F
-		cat $F
-		echo
-	done
+	if [ -d "$1/target/surefire-reports" ]; then
+		for F in $1/target/surefire-reports/*.txt; do
+			echo $F
+			cat $F
+			echo
+		done
+	fi
 }
+
+recurse .
