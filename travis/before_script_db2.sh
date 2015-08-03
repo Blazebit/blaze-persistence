f11d85ed030b6b5cd821a5eb45f315a1419d2cbb
Christian Beikov <christian.beikov@gmail.com>
2015-08-03 11:02:02 +0200

    Try to fix db2 test

--- travis/before_script_db2.sh
+++ travis/before_script_db2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
-sudo -u db2admin -i db2 "CREATE DATABASE TEST"
-sudo -u db2admin -i db2 "ACTIVATE DATABASE TEST"
+sudo -u db2inst1 -i db2 "CREATE DATABASE TEST"
+sudo -u db2inst1 -i db2 "ACTIVATE DATABASE TEST"
+export ADDITIONAL_PROPERTIES=-Djdbc.user=dasusr1 -Djdbc.password=dasusr1
