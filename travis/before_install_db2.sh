ae15b04dd5942ca4b8d270c2503de91191210f33
Christian Beikov <christian.beikov@gmail.com>
2015-08-03 14:06:15 +0200

    Try different password change approach

--- travis/before_install_db2.sh
+++ travis/before_install_db2.sh
@@ -8,9 +8,9 @@ echo "Running db2profile and db2rmln"
 sudo /bin/sh -c '. ~db2inst1/sqllib/db2profile ; $DB2DIR/cfg/db2rmln'
 
 echo "Setting up db2 users"
-echo -e "db2inst1\ndb2inst1" | sudo passwd db2inst1
-echo -e "db2fenc1\ndb2fenc1" | sudo passwd db2fenc1
-echo -e "dasusr1\ndasusr1" | sudo passwd dasusr1
+sudo usermod --password $(echo "db2inst1" | openssl passwd -1 -stdin) db2inst1
+sudo usermod --password $(echo "db2fenc1" | openssl passwd -1 -stdin) db2fenc1
+sudo usermod --password $(echo "dasusr1" | openssl passwd -1 -stdin) dasusr1
 
 mvn -q install:install-file -Dfile=/opt/ibm/db2/V9.7/java/db2jcc4.jar -DgroupId=com.ibm.db2 -DartifactId=db2jcc4 -Dversion=9.7 -Dpackaging=jar -DgeneratePom=true 
 mvn -q install:install-file -Dfile=/opt/ibm/db2/V9.7/java/db2jcc_license_cu.jar -DgroupId=com.ibm.db2 -DartifactId=db2jcc_license_cu -Dversion=9.7 -Dpackaging=jar -DgeneratePom=true
