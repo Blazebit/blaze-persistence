e81f53dbbb69bbc303c539623238256dc6264317
Christian Beikov <christian.beikov@gmail.com>
2015-08-03 12:19:50 +0200

    Try fix db2 travis ci build

--- travis/before_script_db2.sh
+++ travis/before_script_db2.sh
@@ -2,4 +2,4 @@
 
 sudo -u db2inst1 -i db2 "CREATE DATABASE TEST"
 sudo -u db2inst1 -i db2 "ACTIVATE DATABASE TEST"
-export ADDITIONAL_PROPERTIES="-Djdbc.user=dasusr1 -Djdbc.password=dasusr1"
+export ADDITIONAL_PROPERTIES="-Djdbc.user=db2inst1 -Djdbc.password=db2inst1"
