1b9ebd69feef73be4845208ca2ad6da423e2f9c3
Christian Beikov <christian.beikov@gmail.com>
2015-08-03 18:13:29 +0200

    Hopefully final fix for db2

--- travis/before_install_db2.sh
+++ travis/before_install_db2.sh
@@ -12,9 +12,9 @@ sudo usermod --password $(echo "db2inst1" | openssl passwd -1 -stdin) db2inst1
 sudo usermod --password $(echo "db2fenc1" | openssl passwd -1 -stdin) db2fenc1
 sudo usermod --password $(echo "dasusr1" | openssl passwd -1 -stdin) dasusr1
 
-db2set DB2_COMPATIBILITY_VECTOR=MYS
-db2stop
-db2start
+sudo /bin/sh -c '. ~db2inst1/sqllib/bin/db2set DB2_COMPATIBILITY_VECTOR=MYS'
+sudo /bin/sh -c '. ~db2inst1/sqllib/bin/db2stop'
+sudo /bin/sh -c '. ~db2inst1/sqllib/bin/db2start'
 
 mvn -q install:install-file -Dfile=/opt/ibm/db2/V9.7/java/db2jcc4.jar -DgroupId=com.ibm.db2 -DartifactId=db2jcc4 -Dversion=9.7 -Dpackaging=jar -DgeneratePom=true 
 mvn -q install:install-file -Dfile=/opt/ibm/db2/V9.7/java/db2jcc_license_cu.jar -DgroupId=com.ibm.db2 -DartifactId=db2jcc_license_cu -Dversion=9.7 -Dpackaging=jar -DgeneratePom=true
