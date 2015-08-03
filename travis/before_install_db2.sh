bc370ce4d2ddb7a600d881bc396338f5327b1ea5
Christian Beikov <christian.beikov@gmail.com>
2015-08-03 19:25:14 +0200

    Maybe a direct call will work?

--- travis/before_install_db2.sh
+++ travis/before_install_db2.sh
@@ -13,9 +13,18 @@ sudo usermod --password $(echo "db2fenc1" | openssl passwd -1 -stdin) db2fenc1
 sudo usermod --password $(echo "dasusr1" | openssl passwd -1 -stdin) dasusr1
 
 echo "Enable MySQL compatibility"
-sudo /bin/sh -c '. /opt/IBM/db2/V9.7/instance/db2set DB2_COMPATIBILITY_VECTOR=MYS'
-sudo /bin/sh -c '. /opt/IBM/db2/V9.7/instance/db2stop'
-sudo /bin/sh -c '. /opt/IBM/db2/V9.7/instance/db2start'
+sudo /opt/IBM/db2/V9.7/instance/db2set DB2_COMPATIBILITY_VECTOR=MYS
+sudo /opt/IBM/db2/V9.7/instance/db2stop
+sudo /opt/IBM/db2/V9.7/instance/db2start
+
+sudo /opt/IBM/db2/V9.7/bin/db2set DB2_COMPATIBILITY_VECTOR=MYS
+sudo /opt/IBM/db2/V9.7/bin/db2stop
+sudo /opt/IBM/db2/V9.7/bin/db2start
+
+sudo ~db2inst1/sqllib/bin/db2set DB2_COMPATIBILITY_VECTOR=MYS
+sudo ~db2inst1/sqllib/bin/db2stop
+sudo ~db2inst1/sqllib/bin/db2start
+
 
 mvn -q install:install-file -Dfile=/opt/ibm/db2/V9.7/java/db2jcc4.jar -DgroupId=com.ibm.db2 -DartifactId=db2jcc4 -Dversion=9.7 -Dpackaging=jar -DgeneratePom=true 
 mvn -q install:install-file -Dfile=/opt/ibm/db2/V9.7/java/db2jcc_license_cu.jar -DgroupId=com.ibm.db2 -DartifactId=db2jcc_license_cu -Dversion=9.7 -Dpackaging=jar -DgeneratePom=true
