8262f27ce375cc373659e0d1392000e01436ecfc
Christian Beikov <christian.beikov@gmail.com>
2015-08-18 17:50:51 +0200

    Add firebird and oracle for #130

---
+++ travis/before_script_firebird.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+isql-fb -z -q -i /dev/null # --version
+echo 'CREATE DATABASE "LOCALHOST:/tmp/test.fdb" PAGE_SIZE = 16384;' > /tmp/create_test.sql
+isql-fb -u SYSDBA -p masterkey -i /tmp/create_test.sql -q
+cat /tmp/createi_test.sql
