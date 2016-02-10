619b4caca84718708bcd6998d0493738f990629f
Christian Beikov <christian.beikov@gmail.com>
2016-02-10 16:19:00 +0100

    Update mockito version, try adding JAVA_HOME as env variable in matrix

--- travis/before_script_jdk9.sh
+++ travis/before_script_jdk9.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -e
 
-# Disable update alternatives since it doesn't seem to work
-#sudo update-java-alternatives -s java-9-oracle
-export JAVA_HOME=/usr/lib/jvm/java-9-oracle
+sudo update-java-alternatives -s java-9-oracle
