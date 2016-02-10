62f75c7ea3f77705ceabb0242e33698310c2fbcd
Christian Beikov <christian.beikov@gmail.com>
2016-02-10 14:50:13 +0100

    Documentation tab fixes, jdk9 build fix, increase maven memory

--- travis/before_script_jdk9.sh
+++ travis/before_script_jdk9.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
-sudo update-java-alternatives -s java-9-oracle
+# Disable update alternatives since it doesn't seem to work
+#sudo update-java-alternatives -s java-9-oracle
 export JAVA_HOME=/usr/lib/jvm/java-9-oracle
