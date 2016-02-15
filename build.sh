ddc29605316e0128b1b417f9ff39ab3d3d13f6c6
Christian Beikov <christian.beikov@gmail.com>
2016-02-15 22:22:59 +0100

    Temporarily disable other tests, use latest maven version for hibernate,db2

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -e
 if [ "$JDK" = "9" ]; then
   MVN_BIN=/tmp/apache-maven/bin/mvn
   export MAVEN_OPTS="-Xmx1024m -XX:MaxMetaspaceSize=512m"
+elif [ "$LATEST_MAVEN" = "true" ]; then
+  MVN_BIN=/tmp/apache-maven/bin/mvn
+  export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=512m"
 else
   MVN_BIN=mvn
   export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=512m"
