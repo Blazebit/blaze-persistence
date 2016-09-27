ac95c872320ee06337a688099f595a4931827083
Moritz Becker <moritz.becker@gmx.at>
2016-09-27 22:22:22 +0200

    updated docker mysql version

--- docker_db.sh
+++ docker_db.sh
@@ -2,7 +2,7 @@
 
 mysql() {
 	docker rm -f mysql || true
-	docker run --name mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=test -p3306:3306 -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+	docker run --name mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=test -p3306:3306 -d mysql:5.6.25 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 }
 
 if [ -z ${1} ]; then
