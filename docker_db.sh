5c557305fd1a8b38eed3a06d5d4521eed4d948f2
Moritz Becker <moritz.becker@gmx.at>
2016-09-30 13:44:15 +0200

    added separate docker script functions for mysql 5.6 + 5.7

--- docker_db.sh
+++ docker_db.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
 
-mysql() {
+mysql_5_6() {
 	docker rm -f mysql || true
 	docker run --name mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=test -p3306:3306 -d mysql:5.6.25 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+}
+
+mysql_5_7() {
+	docker rm -f mysql || true
+	docker run --name mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=test -p3306:3306 -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 }
 
 db2() {
@@ -14,7 +19,8 @@ db2() {
 if [ -z ${1} ]; then
 	echo "No db name provided"
 	echo "Provide one of:"
-	echo -e "\tmysql"
+	echo -e "\tmysql_5_6"
+	echo -e "\tmysql_5_7"
 	echo -e "\tdb2"
 else
 	${1}
