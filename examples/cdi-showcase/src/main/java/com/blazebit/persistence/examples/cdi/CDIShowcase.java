f9810c867c31d2785cf116786b9105f6a4b63d69
Moritz Becker <moritz.becker@gmx.at>
2016-10-17 19:06:14 +0200

    added cdi showcase

---
+++ examples/cdi-showcase/src/main/java/com/blazebit/persistence/examples/cdi/CDIShowcase.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2014 Blazebit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blazebit.persistence.examples.cdi;
+
+import org.apache.deltaspike.cdise.api.CdiContainer;
+import org.apache.deltaspike.cdise.api.CdiContainerLoader;
+import org.apache.deltaspike.cdise.api.ContextControl;
+import org.apache.deltaspike.core.api.provider.BeanProvider;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * @author Moritz Becker (moritz.becker@gmx.at)
+ * @since 1.2
+ */
+public class CDIShowcase {
+
+    public static void main(String[] args) {
+        CdiContainer cdiContainer = CdiContainerLoader.getCdiContainer();
+        cdiContainer.boot();
+
+        ContextControl contextControl = cdiContainer.getContextControl();
+        contextControl.startContext(ApplicationScoped.class);
+
+        Application application = BeanProvider.getContextualReference(Application.class);
+        application.run();
+
+        cdiContainer.shutdown();
+    }
+
+}
---
+++ examples/cdi-showcase/src/test/java/com/blazebit/persistence/examples/cdi/CDIShowcaseTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2014 Blazebit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blazebit.persistence.examples.cdi;
+
+import org.apache.deltaspike.cdise.api.CdiContainer;
+import org.apache.deltaspike.cdise.api.CdiContainerLoader;
+import org.apache.deltaspike.cdise.api.ContextControl;
+import org.apache.deltaspike.core.api.provider.BeanProvider;
+import org.junit.*;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * @author Moritz Becker (moritz.becker@gmx.at)
+ * @since 1.2
+ */
+public class CDIShowcaseTest {
+
+    @BeforeClass
+    public static void startContainer() {
+        CdiContainer cdiContainer = CdiContainerLoader.getCdiContainer();
+        cdiContainer.boot();
+    }
+
+    @AfterClass
+    public static void stopContainer() {
+        CdiContainer cdiContainer = CdiContainerLoader.getCdiContainer();
+        cdiContainer.shutdown();
+    }
+
+    @Before
+    public void startContexts() {
+        CdiContainer cdiContainer = CdiContainerLoader.getCdiContainer();
+        ContextControl contextControl = cdiContainer.getContextControl();
+        contextControl.startContext(ApplicationScoped.class);
+    }
+
+    @After
+    public void stopContexts() {
+        CdiContainer cdiContainer = CdiContainerLoader.getCdiContainer();
+        ContextControl contextControl = cdiContainer.getContextControl();
+        contextControl.stopContext(ApplicationScoped.class);
+    }
+
+    @Test
+    public void simpleApplicationTest() {
+        // just run the Application's run method - if no exception occur, we that it works
+        Application application = BeanProvider.getContextualReference(Application.class);
+        application.run();
+    }
+
+
+}
