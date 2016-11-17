50ee4e1f7adb08adb50ee3189207bc75d75ae9f4
Christian Beikov <christian.beikov@gmail.com>
2016-11-17 08:20:40 +0100

    Fixed #290

--- build-release-website.sh
+++ build-release-website.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-mvn -P "blazebit-release" --projects website -am clean compile
+if [ "$1" == "" ]; then
+	echo "Stage (local, prod, staging) [local]: "
+	read input </dev/tty
+	
+	if [ "$input" == "" ]; then
+		STAGE="local"
+	elif [ "$input" == "prod" ]; then
+		STAGE="blazebit-release"
+	elif [ "$input" == "staging" ]; then
+		STAGE="staging"
+	else
+		STAGE=$input
+	fi
+else
+	STAGE=$1
+fi
+
+mvn -P $STAGE --projects website -am clean compile
