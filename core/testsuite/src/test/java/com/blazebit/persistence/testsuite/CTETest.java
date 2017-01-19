cae973643521b7f60e6b5696abf4c7a74a0f9ec2
Mobe91 <moritz.becker@gmx.at>
2017-01-19 13:56:33 +0100

    added cte set operation builder test case

--- core/testsuite/src/test/java/com/blazebit/persistence/testsuite/CTETest.java
+++ core/testsuite/src/test/java/com/blazebit/persistence/testsuite/CTETest.java
@@ -680,6 +680,34 @@ public class CTETest extends AbstractCoreTest {
 
     @Test
     @Category({ NoDatanucleus.class, NoEclipselink.class, NoOpenJPA.class, NoMySQL.class })
+    public void testWithStartSetEmptyLeftSideLeaf() {
+        CriteriaBuilder<TestCTE> cb = cbf.create(em, TestCTE.class).withStartSet(TestCTE.class)
+            .from(RecursiveEntity.class, "e")
+            .bind("id").select("e.id")
+            .bind("name").select("e.name")
+            .bind("level").select("0")
+        .exceptAll()
+            .from(RecursiveEntity.class, "e")
+            .bind("id").select("e.id")
+            .bind("name").select("e.name")
+            .bind("level").select("0")
+        .endSet()
+        .startUnionAll().endSet()
+        .endSet().end();
+
+        String expected = ""
+                + "WITH " + TestCTE.class.getSimpleName() + "(id, name, level) AS(\n"
+                + "SELECT e.id, e.name, 0 FROM RecursiveEntity e\n"
+                + "EXCEPT ALL\n"
+                + "SELECT e.id, e.name, 0 FROM RecursiveEntity e\n"
+                + ")\n"
+                + "SELECT testcte FROM " + TestCTE.class.getSimpleName() + " testcte";
+        assertEquals(expected, cb.getQueryString());
+        cb.getResultList();
+    }
+
+    @Test
+    @Category({ NoDatanucleus.class, NoEclipselink.class, NoOpenJPA.class, NoMySQL.class })
     public void testBuilderEndTracking() {
         FullSelectCTECriteriaBuilder<CriteriaBuilder<TestCTE>> cb = cbf.create(em, TestCTE.class).with(TestCTE.class);
         cb.from(RecursiveEntity.class, "e")
