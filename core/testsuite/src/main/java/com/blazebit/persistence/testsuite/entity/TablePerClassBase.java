b34381a271243bb0c4ef4f5dd1e1ba5e80e9b07f
Christian Beikov <christian.beikov@gmail.com>
2017-02-10 16:54:38 +0100

    Added test and fix for single table inheritance

---
+++ core/testsuite/src/main/java/com/blazebit/persistence/testsuite/entity/TablePerClassBase.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2014 - 2017 Blazebit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blazebit.persistence.testsuite.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Christian Beikov
+ * @since 1.0
+ */
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class TablePerClassBase implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    public TablePerClassBase() {
+    }
+
+    public TablePerClassBase(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ core/testsuite/src/main/java/com/blazebit/persistence/testsuite/entity/TablePerClassSub2.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2014 - 2017 Blazebit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blazebit.persistence.testsuite.entity;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Christian Beikov
+ * @since 1.2.0
+ */
+@Entity
+public class TablePerClassSub2 extends TablePerClassBase {
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+
+    public TablePerClassSub2() {
+    }
+
+    public TablePerClassSub2(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
