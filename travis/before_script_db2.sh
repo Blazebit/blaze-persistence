26ad25f9b98da76265c2f741f6927304916f2e24
Christian Beikov <christian.beikov@gmail.com>
2017-02-18 16:22:40 +0100

    Switch db2 matrix entry to use docker

--- travis/before_script_db2.sh
+++ travis/before_script_db2.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -e
 
-sudo -u db2inst1 -i db2 "CREATE DATABASE TEST"
-sudo -u db2inst1 -i db2 "ACTIVATE DATABASE TEST"
+docker run --name db2 -e DB2INST1_PASSWORD=db2inst1-pwd -e LICENSE=accept -p50000:50000 -d ibmcom/db2express-c:10.5.0.5-3.10.0 db2start
+# Give the container some time to start
+sleep 5
+docker exec -t db2 sudo -u db2inst1 /home/db2inst1/sqllib/bin/db2 create database test
+
+docker cp db2:/home/db2inst1/sqllib/java/db2jcc4.jar db2jcc4.jar
+docker cp db2:/home/db2inst1/sqllib/java/db2jcc_license_cu.jar db2jcc_license_cu.jar
+mvn -q install:install-file -Dfile=db2jcc4.jar -DgroupId=com.ibm.db2 -DartifactId=db2jcc4 -Dversion=9.7 -Dpackaging=jar -DgeneratePom=true
+mvn -q install:install-file -Dfile=db2jcc_license_cu.jar -DgroupId=com.ibm.db2 -DartifactId=db2jcc_license_cu -Dversion=9.7 -Dpackaging=jar -DgeneratePom=true
