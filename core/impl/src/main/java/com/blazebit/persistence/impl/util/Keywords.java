47b567e4951c43a31990fd90ace16cb4b541a1a6
Christian Beikov <christian.beikov@gmail.com>
2018-08-02 00:26:15 +0200

    Use set for constant rather than linear time lookup performance

--- core/impl/src/main/java/com/blazebit/persistence/impl/util/Keywords.java
+++ core/impl/src/main/java/com/blazebit/persistence/impl/util/Keywords.java
@@ -17,7 +17,8 @@
 package com.blazebit.persistence.impl.util;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 /**
  * @author Giovanni Lovato
@@ -28,13 +29,13 @@ public final class Keywords {
     /**
      * JPQL keywords from JPA BNF.
      */
-    public static final List<String> JPQL = Arrays
+    public static final Set<String> JPQL = new HashSet<>(Arrays
         .asList("ABS", "ALL", "AND", "ANY", "AS", "ASC", "AVG", "BETWEEN", "BOTH", "BY", "CASE",
             "COALESCE", "CONCAT", "COUNT", "CURRENT_DATE", "CURRENT_TIME", "CURRENT_TIMESTAMP", "DELETE", "DESC", "DISTINCT",
             "ELSE", "EMPTY", "END", "ENTRY", "ESCAPE", "EXISTS", "FETCH", "FROM", "FUNCTION", "GROUP", "HAVING", "IN", "INDEX",
             "INNER", "IS", "JOIN", "KEY", "LEADING", "LEFT", "LENGTH", "LIKE", "LOCATE", "LOWER", "MAX", "MEMBER", "MIN", "MOD",
             "NEW", "NOT", "NULL", "NULLIF", "OBJECT", "OF", "ON", "OR", "ORDER", "OUTER", "SELECT", "SET", "SIZE", "SOME",
-                "SQRT", "SUBSTRING", "SUM", "TRAILING", "TREAT", "TRIM", "TYPE", "UPDATE", "UPPER", "VALUE", "WHEN", "WHERE");
+                "SQRT", "SUBSTRING", "SUM", "TRAILING", "TREAT", "TRIM", "TYPE", "UPDATE", "UPPER", "VALUE", "WHEN", "WHERE"));
 
     private Keywords() {
     }
