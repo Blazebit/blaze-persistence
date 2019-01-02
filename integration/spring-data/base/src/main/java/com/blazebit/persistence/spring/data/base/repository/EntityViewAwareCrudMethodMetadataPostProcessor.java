34911b5a3e89dc236796755c4d2bb98b36ad0f55
Christian Beikov <christian.beikov@gmail.com>
2019-01-02 09:13:26 +0100

    Update license headers

--- integration/spring-data/base/src/main/java/com/blazebit/persistence/spring/data/base/repository/EntityViewAwareCrudMethodMetadataPostProcessor.java
+++ integration/spring-data/base/src/main/java/com/blazebit/persistence/spring/data/base/repository/EntityViewAwareCrudMethodMetadataPostProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 - 2018 Blazebit.
+ * Copyright 2014 - 2019 Blazebit.
  * Copyright 2011-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
