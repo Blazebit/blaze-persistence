539b47851cbd611046ba63a6071143f211a44fea
Christian Beikov <christian.beikov@gmail.com>
2019-01-02 09:13:26 +0100

    Update license headers

--- integration/spring-data/base/src/main/java/com/blazebit/persistence/spring/data/base/query/AbstractPartTreeBlazePersistenceQuery.java
+++ integration/spring-data/base/src/main/java/com/blazebit/persistence/spring/data/base/query/AbstractPartTreeBlazePersistenceQuery.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 - 2018 Blazebit.
+ * Copyright 2014 - 2019 Blazebit.
  * Copyright 2010-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
