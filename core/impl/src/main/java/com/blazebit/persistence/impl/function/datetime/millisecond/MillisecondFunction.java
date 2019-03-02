87a17cf0111076946ac4658af7ef06e9c859fe8f
Jan-Willem Gmelig Meyling <jan-willem@youngmediaexperts.nl>
2019-03-02 16:45:38 +0100

    [#380] Add missing date extraction and truncate functions
    Co-authored-by: Sayra Ranjha <S.S.Ranjha@student.tudelft.nl>

---
+++ core/impl/src/main/java/com/blazebit/persistence/impl/function/datetime/millisecond/MillisecondFunction.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2014 - 2019 Blazebit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blazebit.persistence.impl.function.datetime.millisecond;
+
+import com.blazebit.persistence.spi.FunctionRenderContext;
+import com.blazebit.persistence.spi.JpqlFunction;
+import com.blazebit.persistence.spi.TemplateRenderer;
+
+/**
+ *
+ * @author Jan-Willem Gmelig Meyling
+ * @since 1.4.0
+ */
+public class MillisecondFunction implements JpqlFunction {
+
+    private final TemplateRenderer renderer;
+
+    public MillisecondFunction() {
+        this.renderer = new TemplateRenderer("extract(millisecond from ?1)");
+    }
+
+    public MillisecondFunction(String template) {
+        this.renderer = new TemplateRenderer(template);
+    }
+
+    @Override
+    public boolean hasArguments() {
+        return true;
+    }
+
+    @Override
+    public boolean hasParenthesesIfNoArguments() {
+        return true;
+    }
+
+    @Override
+    public Class<?> getReturnType(Class<?> firstArgumentType) {
+        return Integer.class;
+    }
+
+    @Override
+    public void render(FunctionRenderContext context) {
+        if (context.getArgumentsSize() != 1) {
+            throw new RuntimeException("The second function needs exactly one argument <datetime>! args=" + context);
+        }
+
+        renderer.start(context).addArgument(0).build();
+    }
+}
