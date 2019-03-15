9e8bd017aacde691a37bf642865bd168d7af4ff3
Moritz Becker <moritz.becker@gmx.at>
2019-03-15 09:56:20 +0100

    [#750] explicitly name test tables, add test ignores for jpa providers that do not support map key dereferencing

--- core/testsuite/src/test/java/com/blazebit/persistence/testsuite/AliasBasedMapKeyDereferencingTest.java
+++ core/testsuite/src/test/java/com/blazebit/persistence/testsuite/AliasBasedMapKeyDereferencingTest.java
@@ -18,6 +18,11 @@ package com.blazebit.persistence.testsuite;
 
 import com.blazebit.persistence.CriteriaBuilder;
 import com.blazebit.persistence.testsuite.base.jpa.category.NoDatanucleus;
+import com.blazebit.persistence.testsuite.base.jpa.category.NoEclipselink;
+import com.blazebit.persistence.testsuite.base.jpa.category.NoHibernate42;
+import com.blazebit.persistence.testsuite.base.jpa.category.NoHibernate43;
+import com.blazebit.persistence.testsuite.base.jpa.category.NoHibernate50;
+import com.blazebit.persistence.testsuite.base.jpa.category.NoHibernate51;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -25,6 +30,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,16 +44,16 @@ public class AliasBasedMapKeyDereferencingTest extends AbstractCoreTest {
 
     @Override
     protected Class<?>[] getEntityClasses() {
-        return concat(super.getEntityClasses(), new Class<?>[] {
+        return concat(super.getEntityClasses(),
                 RootEntity.class,
                 MapContainerEntity.class,
                 MapKeyEntity.class,
                 MapValueEntity.class
-        });
+        );
     }
 
     @Test
-    @Category(NoDatanucleus.class)
+    @Category({NoDatanucleus.class, NoHibernate42.class, NoHibernate43.class, NoHibernate50.class, NoHibernate50.class, NoHibernate51.class, NoEclipselink.class})
     public void test() {
         CriteriaBuilder<RootEntity> crit = cbf.create(em, RootEntity.class, "root")
                 .innerJoin("container", "container_1")
@@ -58,6 +64,7 @@ public class AliasBasedMapKeyDereferencingTest extends AbstractCoreTest {
     }
 
     @Entity
+    @Table(name = "root_entity")
     public static class RootEntity {
         @Id
         private Long id;
@@ -66,6 +73,7 @@ public class AliasBasedMapKeyDereferencingTest extends AbstractCoreTest {
     }
 
     @Entity
+    @Table(name = "map_container_entity")
     public static class MapContainerEntity {
         @Id
         private Long id;
@@ -74,12 +82,14 @@ public class AliasBasedMapKeyDereferencingTest extends AbstractCoreTest {
     }
 
     @Entity
+    @Table(name = "map_key_entity")
     public static class MapKeyEntity {
         @Id
         private Long id;
     }
 
     @Entity
+    @Table(name = "map_value_entity")
     public static class MapValueEntity {
         @Id
         private Long id;
