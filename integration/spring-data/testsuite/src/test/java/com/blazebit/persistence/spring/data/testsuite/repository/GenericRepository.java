96ed482365a59de8db680286ac417a71c1587f83
Christian Beikov <christian.beikov@gmail.com>
2019-09-21 12:38:12 +0200

    [#848] Update blaze-utils to fix an issue with unassigned type variables

---
+++ integration/spring-data/testsuite/src/test/java/com/blazebit/persistence/spring/data/testsuite/repository/GenericRepository.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2014 - 2019 Blazebit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blazebit.persistence.spring.data.testsuite.repository;
+
+import com.blazebit.persistence.spring.data.repository.EntityViewRepository;
+import com.blazebit.persistence.spring.data.testsuite.view.DocumentView;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author Christian Beikov
+ * @since 1.4.0
+ */
+@Repository
+@Transactional(readOnly = true)
+public interface GenericRepository<T extends DocumentView> extends EntityViewRepository<T, Long> {
+
+}
