8b9ac5f6499359de1b52cc355e1afa33241fef77
PhilEder <p.eder92@gmx.at>
2020-01-26 19:22:55 +0100

    [Blazebit#840] checking creatable entity views for abstract entity mappings

--- entity-view/testsuite/src/test/java/com/blazebit/persistence/view/testsuite/basic/AbstractMappingTest.java
+++ entity-view/testsuite/src/test/java/com/blazebit/persistence/view/testsuite/basic/AbstractMappingTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 - 2020 Blazebit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.blazebit.persistence.view.testsuite.basic;
 
 import com.blazebit.persistence.testsuite.entity.IntIdEntity;
@@ -12,8 +28,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * @author Philipp Eder <p.eder@curecomp.com>
- * @since 24.01.2020
+ * @author Philipp Eder
+ * @since 1.4.1
  */
 public class AbstractMappingTest extends AbstractEntityViewTest {
 
