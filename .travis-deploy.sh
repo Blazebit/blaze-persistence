02c4966eebc8bfeed112c66b38262beadb1b87ec
Christian Beikov <christian.beikov@gmail.com>
2020-05-22 19:30:44 +0200

    Try fix snapshot deployment

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+set -x
 
 if [ "$TRAVIS_REPO_SLUG" == "Blazebit/blaze-persistence" ] && 
     [ "$TRAVIS_BRANCH" == "master" ] &&
@@ -7,7 +9,7 @@ if [ "$TRAVIS_REPO_SLUG" == "Blazebit/blaze-persistence" ] &&
 
   echo "Starting snapshot deployment..."
   export MAVEN_OPTS="$MAVEN_OPTS -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn"
-  exec mvn -B -P blazebit-release -s .travis-settings.xml -DperformRelease -DskipTests -DskipITs -Dgpg.skip=true -Dquiet=true -Djdk8.home=/usr/lib/jvm/java-8-oracle clean deploy
+  exec mvn -P blazebit-release -s .travis-settings.xml -DperformRelease -DskipTests -DskipITs -Dgpg.skip=true -Dquiet=true -Djdk8.home=/usr/lib/jvm/java-8-oracle clean deploy
 else
   echo "Skipping snapshot deployment..."
 fi
