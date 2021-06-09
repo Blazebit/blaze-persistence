a6e07e7c34eef09d3b4733bc091e44ad575f718d
Jan-Willem Gmelig Meyling <jan-willem@youngmediaexperts.nl>
2021-06-09 17:53:08 +0200

    [#1313] Test case showing invalid SQL being generated when inlining subquery CTE

---
+++ core/testsuite/src/main/java/com/blazebit/persistence/testsuite/entity/DocumentCTE.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2014 - 2021 Blazebit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blazebit.persistence.testsuite.entity;
+
+import com.blazebit.persistence.CTE;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Christian Beikov
+ * @since 1.1.0
+ */
+@Entity
+@CTE
+public class DocumentCTE implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private Long id;
+
+    private Document document;
+
+    private Document parent;
+
+    private Document root;
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    public Document getDocument() {
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+
+    @ManyToOne
+    public Document getParent() {
+        return parent;
+    }
+
+    public void setParent(Document parent) {
+        this.parent = parent;
+    }
+
+    @ManyToOne
+    public Document getRoot() {
+        return root;
+    }
+
+    public void setRoot(Document root) {
+        this.root = root;
+    }
+}
