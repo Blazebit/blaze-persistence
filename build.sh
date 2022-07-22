130be0f52b419619cd6c2c57bf031b4ffda63fc1
Christian Beikov <christian.beikov@gmail.com>
2022-07-22 15:35:56 +0200

    Try to get rid of some build complexity

--- build.sh
+++ build.sh
@@ -50,35 +50,41 @@ if [ "$JDK" != "" ]; then
   PROPERTIES="$PROPERTIES -Djdk8.home=$JDK8_HOME"
 fi
 
-if { [ "$JPAPROVIDER" == "hibernate-5.2" ] || [ "$JPAPROVIDER" == "hibernate-apt" ]; } &&
-    [ "$RDBMS" == "h2" ]; then
-  exec $DIR/mvnw -B -P ${JPAPROVIDER},${RDBMS},${SPRING_DATA:-spring-data-1.11.x},${DELTASPIKE:-deltaspike-1.7} clean install -V $PROPERTIES
+#if { [ "$JPAPROVIDER" == "hibernate-5.2" ] || [ "$JPAPROVIDER" == "hibernate-apt" ]; } &&
+#    [ "$RDBMS" == "h2" ]; then
+#  exec $DIR/mvnw -B -P ${JPAPROVIDER},${RDBMS},${SPRING_DATA:-spring-data-1.11.x},${DELTASPIKE:-deltaspike-1.7} clean install -V $PROPERTIES
+#else
+#  PROJECT_LIST="core/testsuite,entity-view/testsuite,jpa-criteria/testsuite,integration/deltaspike-data/testsuite,integration/jaxrs,integration/spring-data/testsuite/webflux,integration/spring-data/testsuite/webmvc,examples/spring-data-webmvc,examples/spring-data-webflux,examples/showcase/runner/spring,examples/showcase/runner/cdi,integration/querydsl/testsuite,integration/spring-hateoas/webmvc"
+#  if [ "$JPAPROVIDER" == "hibernate-6.0" ] &&
+#    [ "$RDBMS" == "h2" ]; then
+#    # Just in case we want to run against a specific version
+##    cd ..
+##    git clone --depth=1 --branch="bp-integration" https://github.com/beikov/hibernate-orm.git hibernate6
+##    cd hibernate6
+##    ./gradlew -Dmaven.repo.local=$HOME/.m2/repository build publishToMavenLocal -x :documentation:buildDocs -x :hibernate-core:javadoc -x :hibernate-jpamodelgen:javadoc -x :documentation:aggregateJavadocs -x test -x javadoc -x checkStyleMain -x checkStyleTest
+##    cd -
+##    cd ../blaze-persistence
+#    PROJECT_LIST="$PROJECT_LIST,core/testsuite-jakarta-runner,entity-view/testsuite-jakarta-runner,jpa-criteria/testsuite-jakarta-runner"
+#    exec $DIR/mvnw -B -P ${JPAPROVIDER},${RDBMS},${SPRING_DATA:-spring-data-1.11.x},${DELTASPIKE:-deltaspike-1.7} clean install --projects $PROJECT_LIST -am -V $PROPERTIES
+#  elif [ "$JPAPROVIDER" == "hibernate-5.4" ]; then
+#    PROJECT_LIST="$PROJECT_LIST,integration/quarkus/deployment,examples/quarkus/testsuite/base"
+#    if [ "$NATIVE" == "true" ]; then
+#      if [ "$RDBMS" == "mysql8" ]; then
+#        PROJECT_LIST="$PROJECT_LIST,examples/quarkus/testsuite/native/mysql"
+#      else
+#        PROJECT_LIST="$PROJECT_LIST,examples/quarkus/testsuite/native/$RDBMS"
+#      fi
+#      exec $DIR/mvnw -B -P ${JPAPROVIDER},${RDBMS},${SPRING_DATA:-spring-data-1.11.x},${DELTASPIKE:-deltaspike-1.7},native clean install --projects $PROJECT_LIST -am -V $PROPERTIES
+#    else
+#      exec $DIR/mvnw -B -P ${JPAPROVIDER},${RDBMS},${SPRING_DATA:-spring-data-1.11.x},${DELTASPIKE:-deltaspike-1.7} clean install --projects $PROJECT_LIST -am -V $PROPERTIES
+#    fi
+#  else
+#    exec $DIR/mvnw -B -P ${JPAPROVIDER},${RDBMS},${SPRING_DATA:-spring-data-1.11.x},${DELTASPIKE:-deltaspike-1.7} clean install --projects $PROJECT_LIST -am -V $PROPERTIES
+#  fi
+#fi
+
+if [ "$NATIVE" == "true" ]; then
+  exec $DIR/mvnw -B -P ${JPAPROVIDER},${RDBMS},${SPRING_DATA:-spring-data-1.11.x},${DELTASPIKE:-deltaspike-1.7},native clean install -am -V $PROPERTIES
 else
-  PROJECT_LIST="core/testsuite,entity-view/testsuite,jpa-criteria/testsuite,integration/deltaspike-data/testsuite,integration/jaxrs,integration/spring-data/testsuite/webflux,integration/spring-data/testsuite/webmvc,examples/spring-data-webmvc,examples/spring-data-webflux,examples/showcase/runner/spring,examples/showcase/runner/cdi,integration/querydsl/testsuite,integration/spring-hateoas/webmvc"
-  if [ "$JPAPROVIDER" == "hibernate-6.0" ] &&
-    [ "$RDBMS" == "h2" ]; then
-    # Just in case we want to run against a specific version
-#    cd ..
-#    git clone --depth=1 --branch="bp-integration" https://github.com/beikov/hibernate-orm.git hibernate6
-#    cd hibernate6
-#    ./gradlew -Dmaven.repo.local=$HOME/.m2/repository build publishToMavenLocal -x :documentation:buildDocs -x :hibernate-core:javadoc -x :hibernate-jpamodelgen:javadoc -x :documentation:aggregateJavadocs -x test -x javadoc -x checkStyleMain -x checkStyleTest
-#    cd -
-#    cd ../blaze-persistence
-    PROJECT_LIST="$PROJECT_LIST,core/testsuite-jakarta-runner,entity-view/testsuite-jakarta-runner,jpa-criteria/testsuite-jakarta-runner"
-    exec $DIR/mvnw -B -P ${JPAPROVIDER},${RDBMS},${SPRING_DATA:-spring-data-1.11.x},${DELTASPIKE:-deltaspike-1.7} clean install --projects $PROJECT_LIST -am -V $PROPERTIES
-  elif [ "$JPAPROVIDER" == "hibernate-5.4" ]; then
-    PROJECT_LIST="$PROJECT_LIST,integration/quarkus/deployment,examples/quarkus/testsuite/base"
-    if [ "$NATIVE" == "true" ]; then
-      if [ "$RDBMS" == "mysql8" ]; then
-        PROJECT_LIST="$PROJECT_LIST,examples/quarkus/testsuite/native/mysql"
-      else
-        PROJECT_LIST="$PROJECT_LIST,examples/quarkus/testsuite/native/$RDBMS"
-      fi
-      exec $DIR/mvnw -B -P ${JPAPROVIDER},${RDBMS},${SPRING_DATA:-spring-data-1.11.x},${DELTASPIKE:-deltaspike-1.7},native clean install --projects $PROJECT_LIST -am -V $PROPERTIES
-    else
-      exec $DIR/mvnw -B -P ${JPAPROVIDER},${RDBMS},${SPRING_DATA:-spring-data-1.11.x},${DELTASPIKE:-deltaspike-1.7} clean install --projects $PROJECT_LIST -am -V $PROPERTIES
-    fi
-  else
-    exec $DIR/mvnw -B -P ${JPAPROVIDER},${RDBMS},${SPRING_DATA:-spring-data-1.11.x},${DELTASPIKE:-deltaspike-1.7} clean install --projects $PROJECT_LIST -am -V $PROPERTIES
-  fi
+  exec $DIR/mvnw -B -P ${JPAPROVIDER},${RDBMS},${SPRING_DATA:-spring-data-1.11.x},${DELTASPIKE:-deltaspike-1.7} clean install -am -V $PROPERTIES
 fi
