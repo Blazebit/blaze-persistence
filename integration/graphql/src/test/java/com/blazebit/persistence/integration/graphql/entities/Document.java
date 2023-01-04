7cfc941d00995aa929b94855ec7d3d148d71c8a5
David Kubecka <davidkubecka366@gmail.com>
2023-01-04 16:50:10 +0100

    [#1653]: write unit test for GraphQLEntityViewSupport.createSetting
    To facilitate easy test creation in the future, a helper class
    is created to create types and mocks corresponding to the test
    schema.
    
    Also, graphql-java version is bumped to 10 since it contains
    the classes/methods required for the test (namely SelectedField
    and getFields) and the previosly used version 9.* is no longer
    supported. Moreover, the current GraphQLEntityViewSupport code
    doesn't work since 9.7 anyway.

---
+++ integration/graphql/src/test/java/com/blazebit/persistence/integration/graphql/entities/Document.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2014 - 2023 Blazebit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blazebit.persistence.integration.graphql.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+/**
+ * @author Moritz Becker
+ * @since 1.6.4
+ */
+@Entity
+public class Document implements Serializable {
+
+    private Long id;
+    private String name;
+    private Person owner;
+
+    public Document() {
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+}
