08793f4ab72cd57762bb6d222ad1fa9421f23e22
Christian Beikov <christian.beikov@gmail.com>
2023-01-05 18:47:10 +0100

    Update license headers

--- integration/entity-view-spring/src/main/java/com/blazebit/persistence/integration/view/spring/impl/TypeFilterParser.java
+++ integration/entity-view-spring/src/main/java/com/blazebit/persistence/integration/view/spring/impl/TypeFilterParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 - 2022 Blazebit.
+ * Copyright 2014 - 2023 Blazebit.
  * Copyright 2010-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
