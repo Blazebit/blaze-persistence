fa573cf2d883e7553f0ee74b7741e15132084530
Christian Beikov <christian.beikov@gmail.com>
2023-06-05 13:56:32 +0200

    Fix compile errors that popped up during snapshot publishing and quarkus ecosystem ci tests

--- examples/quarkus-3/testsuite/native/mysql/src/test/java/com/blazebit/persistence/examples/quarkus/testsuite/mysql/QuarkusExampleIT.java
+++ examples/quarkus-3/testsuite/native/mysql/src/test/java/com/blazebit/persistence/examples/quarkus/testsuite/mysql/QuarkusExampleIT.java
@@ -17,11 +17,6 @@ package com.blazebit.persistence.examples.quarkus.testsuite.mysql;
 
 import com.blazebit.persistence.examples.quarkus.testsuite.base.AbstractQuarkusExampleTest;
 import io.quarkus.test.junit.NativeImageTest;
-import io.restassured.http.ContentType;
-import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.matchesPattern;
 
 /**
  * @author Moritz Becker
