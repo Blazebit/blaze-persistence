711ea5fad81b01fde9305714a19f1c12167c0984
Moritz Becker <moritz.becker@gmx.at>
2023-07-26 08:08:31 +0200

    Add test for @Any mapping

---
+++ core/testsuite-hibernate6/src/test/java/com/blazebit/persistence/testsuite/AnyMappingTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2014 - 2023 Blazebit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.blazebit.persistence.testsuite;
+
+import static com.blazebit.persistence.testsuite.base.jpa.AbstractJpaPersistenceTest.cbf;
+import static org.junit.Assert.assertTrue;
+
+import com.blazebit.persistence.CriteriaBuilder;
+import com.blazebite.persistence.testsuite.hibernate6.entity.PropertyHolder;
+import org.junit.jupiter.api.Test;
+
+public class AnyMappingTest extends AbstractCoreTest {
+
+    @Override
+    protected Class<?>[] getEntityClasses() {
+        return new Class<?>[] {
+            PropertyHolder.class
+        };
+    }
+
+    @Test
+    public void anyMapping_doesNotFailCriteriaBuilderFactoryCreation() {
+        CriteriaBuilder<PropertyHolder> cb = cbf.create(em, PropertyHolder.class, "p");
+        assertTrue(true);
+    }
+}
