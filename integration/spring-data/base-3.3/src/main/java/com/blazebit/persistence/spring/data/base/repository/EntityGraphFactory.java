cc898ada403f8f95f94a24699700eb5e6c595735
em <eugen.mayer@kontextwork.de>
2024-06-07 14:50:26 +0200

    Create new spring 3.3 integration project and reimplement JpaParameters.
    Add tests for hibernate 6.4/6.5 with spring 3.1/3.3

---
+++ integration/spring-data/base-3.3/src/main/java/com/blazebit/persistence/spring/data/base/repository/EntityGraphFactory.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2021-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.blazebit.persistence.spring.data.base.repository;
+
+import jakarta.persistence.EntityGraph;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Subgraph;
+import org.springframework.data.mapping.PropertyPath;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Factory class to create an {@link EntityGraph} from a collection of property paths.
+ *
+ * @author Jens Schauder
+ * @author Petr Strnad
+ * @author Greg Turnquist
+ * @since 2.6
+ */
+public abstract class EntityGraphFactory {
+
+    public static final String HINT = "jakarta.persistence.fetchgraph";
+
+    /**
+     * Create an {@link EntityGraph} from a collection of properties.
+     *
+     * @param domainType
+     * @param properties
+     */
+    public static <T> EntityGraph<T> create(EntityManager entityManager, Class<T> domainType, Set<String> properties) {
+
+        EntityGraph<T> entityGraph = entityManager.createEntityGraph(domainType);
+        Map<String, Subgraph<Object>> existingSubgraphs = new HashMap<>();
+
+        for (String property : properties) {
+
+            Subgraph<Object> current = null;
+            String currentFullPath = "";
+
+            for (PropertyPath path : PropertyPath.from(property, domainType)) {
+
+                currentFullPath += path.getSegment() + ".";
+
+                if (path.hasNext()) {
+                    final Subgraph<Object> finalCurrent = current;
+                    current = current == null
+                        ? existingSubgraphs.computeIfAbsent(currentFullPath, k -> entityGraph.addSubgraph(path.getSegment()))
+                        : existingSubgraphs.computeIfAbsent(currentFullPath, k -> finalCurrent.addSubgraph(path.getSegment()));
+                    continue;
+                }
+
+                if (current == null) {
+                    entityGraph.addAttributeNodes(path.getSegment());
+                } else {
+                    current.addAttributeNodes(path.getSegment());
+
+                }
+            }
+        }
+
+        return entityGraph;
+    }
+
+}
---
+++ integration/spring-data/base-3.3/src/main/java/org/springframework/data/jpa/repository/query/FixedJpaCountQueryCreator.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2014 - 2024 Blazebit.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.data.jpa.repository.query;
+
+import com.blazebit.persistence.spring.data.base.query.ParameterMetadataProvider;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.parser.PartTree;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+
+/**
+ * @author Moritz Becker
+ * @since 1.2.0
+ */
+public class FixedJpaCountQueryCreator extends FixedJpaQueryCreator {
+
+    public FixedJpaCountQueryCreator(PartTree tree, Class<?> domainClass, CriteriaBuilder builder,
+                                     ParameterMetadataProvider provider) {
+        super(tree, domainClass, builder, provider);
+    }
+
+    @Override
+    protected CriteriaQuery<Object> complete(Predicate predicate, Sort sort, CriteriaQuery<Object> query,
+                                             CriteriaBuilder builder, Root<?> root) {
+        CriteriaQuery<Object> select = query.select(query.isDistinct() ? builder.countDistinct(root) : builder.count(root));
+        return predicate == null ? select : select.where(predicate);
+    }
+}
