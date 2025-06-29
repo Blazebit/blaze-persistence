55bb500af676b69105f1f1c5f26e5db06bc54e62
Christian Beikov <christian.beikov@gmail.com>
2025-06-29 17:47:49 +0200

    [#1958] Add Spring Boot/Data 3.4 and 3.5 support

---
+++ integration/spring-data/3.4/src/main/java/com/blazebit/persistence/spring/data/impl/query/EntityViewAwareRepositoryMetadataImpl.java
@@ -0,0 +1,131 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Blazebit
+ */
+
+package com.blazebit.persistence.spring.data.impl.query;
+
+import com.blazebit.persistence.spring.data.base.query.EntityViewAwareRepositoryMetadata;
+import com.blazebit.persistence.view.EntityViewManager;
+import com.blazebit.persistence.view.metamodel.ManagedViewType;
+import org.springframework.data.repository.core.CrudMethods;
+import org.springframework.data.repository.core.RepositoryMetadata;
+import org.springframework.data.repository.core.support.RepositoryFragment;
+import org.springframework.data.util.TypeInformation;
+
+import java.lang.reflect.Method;
+import java.util.Set;
+
+/**
+ * @author Christian Beikov
+ * @since 1.2.0
+ */
+public class EntityViewAwareRepositoryMetadataImpl implements EntityViewAwareRepositoryMetadata {
+
+    private final RepositoryMetadata metadata;
+    private final EntityViewManager evm;
+    private final Class<?> domainType;
+    private final Class<?> entityViewType;
+
+    public EntityViewAwareRepositoryMetadataImpl(RepositoryMetadata metadata, EntityViewManager evm) {
+        this.metadata = metadata;
+        this.evm = evm;
+        Class<?> domainType = metadata.getDomainType();
+        ManagedViewType<?> managedViewType = evm.getMetamodel().managedView(domainType);
+        if (managedViewType == null) {
+            this.domainType = domainType;
+            this.entityViewType = null;
+        } else {
+            this.domainType = managedViewType.getEntityClass();
+            this.entityViewType = managedViewType.getJavaType();
+        }
+    }
+
+    @Override
+    public EntityViewManager getEntityViewManager() {
+        return evm;
+    }
+
+    @Override
+    public Class<?> getIdType() {
+        return metadata.getIdType();
+    }
+
+    @Override
+    public Class<?> getDomainType() {
+        return domainType;
+    }
+
+    @Override
+    public Class<?> getEntityViewType() {
+        return entityViewType;
+    }
+
+    @Override
+    public Class<?> getRepositoryInterface() {
+        return metadata.getRepositoryInterface();
+    }
+
+    @Override
+    public Class<?> getReturnedDomainClass(Method method) {
+        Class<?> returnedDomainClass = metadata.getReturnedDomainClass(method);
+        ManagedViewType<?> managedViewType = evm.getMetamodel().managedView(returnedDomainClass);
+        if (managedViewType == null) {
+            return returnedDomainClass;
+        } else {
+            return managedViewType.getEntityClass();
+        }
+    }
+
+    @Override
+    public TypeInformation<?> getReturnType(Method method) {
+        return metadata.getReturnType(method);
+    }
+
+    @Override
+    public Class<?> getReturnedEntityViewClass(Method method) {
+        Class<?> returnedDomainClass = metadata.getReturnedDomainClass(method);
+        ManagedViewType<?> managedViewType = evm.getMetamodel().managedView(returnedDomainClass);
+        if (managedViewType == null) {
+            return null;
+        } else {
+            return managedViewType.getJavaType();
+        }
+    }
+
+    @Override
+    public CrudMethods getCrudMethods() {
+        return metadata.getCrudMethods();
+    }
+
+    @Override
+    public boolean isPagingRepository() {
+        return metadata.isPagingRepository();
+    }
+
+    @Override
+    public Set<Class<?>> getAlternativeDomainTypes() {
+        return metadata.getAlternativeDomainTypes();
+    }
+
+    @Override
+    public boolean isReactiveRepository() {
+        return metadata.isReactiveRepository();
+    }
+
+    @Override
+    public TypeInformation<?> getIdTypeInformation() {
+        return metadata.getIdTypeInformation();
+    }
+
+    @Override
+    public TypeInformation<?> getDomainTypeInformation() {
+        return metadata.getDomainTypeInformation();
+    }
+
+    @Override
+    public Set<RepositoryFragment<?>> getFragments() {
+        // TODO: implement
+        return null;
+    }
+}
---
+++ integration/spring-data/3.4/src/main/java/com/blazebit/persistence/spring/data/impl/query/PartTreeBlazePersistenceQuery.java
@@ -0,0 +1,234 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Blazebit
+ */
+
+package com.blazebit.persistence.spring.data.impl.query;
+
+import com.blazebit.persistence.CriteriaBuilderFactory;
+import com.blazebit.persistence.PagedList;
+import com.blazebit.persistence.spring.data.base.query.AbstractPartTreeBlazePersistenceQuery;
+import com.blazebit.persistence.spring.data.base.query.EntityViewAwareJpaQueryMethod;
+import com.blazebit.persistence.spring.data.base.query.JpaParameters;
+import com.blazebit.persistence.spring.data.base.query.KeysetAwarePageImpl;
+import com.blazebit.persistence.spring.data.base.query.KeysetAwareSliceImpl;
+import com.blazebit.persistence.spring.data.base.query.ParameterBinder;
+import com.blazebit.persistence.spring.data.base.query.ParameterMetadataProvider;
+import com.blazebit.persistence.spring.data.repository.KeysetPageable;
+import com.blazebit.persistence.view.EntityViewManager;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.provider.PersistenceProvider;
+import org.springframework.data.jpa.repository.query.AbstractJpaQuery;
+import org.springframework.data.jpa.repository.query.EscapeCharacter;
+import org.springframework.data.jpa.repository.query.Jpa21Utils;
+import org.springframework.data.jpa.repository.query.JpaEntityGraph;
+import org.springframework.data.jpa.repository.query.JpaParametersParameterAccessor;
+import org.springframework.data.jpa.repository.query.JpaQueryExecution;
+import org.springframework.data.jpa.repository.support.QueryHints;
+import org.springframework.data.repository.query.ParametersParameterAccessor;
+import org.springframework.data.repository.query.parser.PartTree;
+import jakarta.persistence.EntityManager;
+
+import jakarta.persistence.Query;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Moritz Becker
+ * @author Christian Beikov
+ * @author Eugen Mayer
+ * @since 1.6.9
+ */
+public class PartTreeBlazePersistenceQuery extends AbstractPartTreeBlazePersistenceQuery {
+
+    public PartTreeBlazePersistenceQuery(EntityViewAwareJpaQueryMethod method, EntityManager em, PersistenceProvider persistenceProvider, EscapeCharacter escape, CriteriaBuilderFactory cbf, EntityViewManager evm) {
+        super(method, em, persistenceProvider, escape, cbf, evm);
+    }
+
+    @Override
+    protected ParameterMetadataProvider createParameterMetadataProvider(CriteriaBuilder builder, ParametersParameterAccessor accessor, PersistenceProvider provider, Object escape) {
+        return new ParameterMetadataProviderImpl(builder, accessor, provider, (EscapeCharacter) escape);
+    }
+
+    @Override
+    protected ParameterMetadataProvider createParameterMetadataProvider(CriteriaBuilder builder, JpaParameters parameters, PersistenceProvider provider, Object escape) {
+        return new ParameterMetadataProviderImpl(builder, parameters, provider, (EscapeCharacter) escape);
+    }
+
+    @Override
+    protected JpaQueryExecution getExecution() {
+        if (getQueryMethod().isSliceQuery()) {
+            return new PartTreeBlazePersistenceQuery.SlicedExecution();
+        } else if (getQueryMethod().isPageQuery()) {
+            return new PartTreeBlazePersistenceQuery.PagedExecution();
+        } else if (isDelete()) {
+            return new PartTreeBlazePersistenceQuery.DeleteExecution(getEntityManager());
+        } else if (isExists()) {
+            return new PartTreeBlazePersistenceQuery.ExistsExecution();
+        } else {
+            return super.getExecution();
+        }
+    }
+
+    /**
+     * {@link JpaQueryExecution} performing an exists check on the query.
+     *
+     * @author Christian Beikov
+     * @since 1.3.0
+     */
+    private static class ExistsExecution extends JpaQueryExecution {
+
+        @Override
+        protected Object doExecute(AbstractJpaQuery repositoryQuery, JpaParametersParameterAccessor jpaParametersParameterAccessor) {
+            return !((PartTreeBlazePersistenceQuery) repositoryQuery).createQuery(jpaParametersParameterAccessor).getResultList().isEmpty();
+        }
+    }
+
+    /**
+     * Uses the {@link com.blazebit.persistence.PaginatedCriteriaBuilder} API for executing the query.
+     *
+     * @author Christian Beikov
+     * @since 1.2.0
+     */
+    private static class SlicedExecution extends JpaQueryExecution {
+
+        @Override
+        @SuppressWarnings("unchecked")
+        protected Object doExecute(AbstractJpaQuery repositoryQuery, JpaParametersParameterAccessor jpaParametersParameterAccessor) {
+            Query paginatedCriteriaBuilder = ((PartTreeBlazePersistenceQuery) repositoryQuery).createPaginatedQuery(jpaParametersParameterAccessor.getValues(), false);
+            PagedList<Object> resultList = (PagedList<Object>) paginatedCriteriaBuilder.getResultList();
+            Pageable pageable = jpaParametersParameterAccessor.getPageable();
+
+            return new KeysetAwareSliceImpl<>(resultList, pageable);
+        }
+    }
+
+    /**
+     * Uses the {@link com.blazebit.persistence.PaginatedCriteriaBuilder} API for executing the query.
+     *
+     * @author Christian Beikov
+     * @since 1.2.0
+     */
+    private static class PagedExecution extends JpaQueryExecution {
+
+        @Override
+        @SuppressWarnings("unchecked")
+        protected Object doExecute(AbstractJpaQuery repositoryQuery, JpaParametersParameterAccessor jpaParametersParameterAccessor) {
+            Pageable pageable = jpaParametersParameterAccessor.getPageable();
+            if (pageable.isUnpaged()) {
+                List<Object> unpagedResult = ((PartTreeBlazePersistenceQuery) repositoryQuery).createQuery(jpaParametersParameterAccessor).getResultList();
+                return new KeysetAwarePageImpl<>(unpagedResult);
+            }
+
+            Query paginatedCriteriaBuilder = ((PartTreeBlazePersistenceQuery) repositoryQuery).createPaginatedQuery(jpaParametersParameterAccessor.getValues(), true);
+            PagedList<Object> resultList = (PagedList<Object>) paginatedCriteriaBuilder.getResultList();
+            Long total = resultList.getTotalSize();
+
+            if (total.equals(0L)) {
+                return new KeysetAwarePageImpl<>(Collections.emptyList(), total, null, pageable);
+            }
+
+            return new KeysetAwarePageImpl<>(resultList, pageable);
+        }
+    }
+
+    /**
+     * {@link JpaQueryExecution} removing entities matching the query.
+     *
+     * @author Thomas Darimont
+     * @author Oliver Gierke
+     * @since 1.6
+     */
+    static class DeleteExecution extends JpaQueryExecution {
+
+        private final EntityManager em;
+
+        public DeleteExecution(EntityManager em) {
+            this.em = em;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see org.springframework.data.jpa.repository.query.JpaQueryExecution#doExecute(org.springframework.data.jpa.repository.query.AbstractJpaQuery, java.lang.Object[])
+         */
+        @Override
+        protected Object doExecute(AbstractJpaQuery jpaQuery, JpaParametersParameterAccessor jpaParametersParameterAccessor) {
+            Query query = ((PartTreeBlazePersistenceQuery) jpaQuery).createQuery(jpaParametersParameterAccessor);
+            List<?> resultList = query.getResultList();
+
+            for (Object o : resultList) {
+                em.remove(o);
+            }
+
+            return jpaQuery.getQueryMethod().isCollectionQuery() ? resultList : resultList.size();
+        }
+    }
+
+    @Override
+    protected Query doCreateQuery(JpaParametersParameterAccessor jpaParametersParameterAccessor) {
+        return super.doCreateQuery(jpaParametersParameterAccessor.getValues());
+    }
+
+    @Override
+    protected Query doCreateCountQuery(JpaParametersParameterAccessor jpaParametersParameterAccessor) {
+        return super.doCreateCountQuery(jpaParametersParameterAccessor.getValues());
+    }
+
+    @Override
+    protected boolean isCountProjection(PartTree tree) {
+        return tree.isCountProjection();
+    }
+
+    @Override
+    protected boolean isDelete(PartTree tree) {
+        return tree.isDelete();
+    }
+
+    @Override
+    protected boolean isExists(PartTree tree) {
+        return tree.isExistsProjection();
+    }
+
+    @Override
+    protected int getOffset(Pageable pageable) {
+        if (pageable.isPaged()) {
+            if (pageable instanceof KeysetPageable) {
+                return ((KeysetPageable) pageable).getIntOffset();
+            } else {
+                return (int) pageable.getOffset();
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    protected int getLimit(Pageable pageable) {
+        if (pageable.isPaged()) {
+            return pageable.getPageSize();
+        }
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    protected ParameterBinder createCriteriaQueryParameterBinder(JpaParameters parameters, Object[] values, List<ParameterMetadataProvider.ParameterMetadata<?>> expressions) {
+        return new CriteriaQueryParameterBinder(getEntityManager(), evm, parameters, values, expressions);
+    }
+
+    @Override
+    protected Map<String, Object> tryGetFetchGraphHints(JpaEntityGraph entityGraph, Class<?> entityType) {
+        QueryHints fetchGraphHint = Jpa21Utils.getFetchGraphHint(
+                this.getEntityManager(),
+                entityGraph,
+                this.getQueryMethod()
+                        .getEntityInformation()
+                        .getJavaType()
+        );
+        Map<String, Object> map = new HashMap<>();
+        fetchGraphHint.forEach(map::put);
+        return map;
+    }
+}
