37cddf51c26c35b346a13bdd6e41c6bcdc613252
msaull <msaull@hotmail.com>
2026-06-17 19:52:38 -0500

    checkstyle

--- core/impl/src/main/java/com/blazebit/persistence/impl/function/tostringjson/OracleToStringJsonFunction.java
+++ core/impl/src/main/java/com/blazebit/persistence/impl/function/tostringjson/OracleToStringJsonFunction.java
@@ -73,11 +73,11 @@ public class OracleToStringJsonFunction extends AbstractToStringJsonFunction {
 
         context.addChunk(field);
 
-        if(subJson){ //treat as JSON to prevent " escaping
+        if (subJson) { // treat as JSON to prevent " escaping
             context.addChunk("':");
             context.addChunk(itemExpression);
             context.addChunk(" FORMAT JSON");
-        }else { //cast to string
+        } else { // cast to string
             context.addChunk("':TO_CHAR(");
             context.addChunk(itemExpression);
             context.addChunk(")");
